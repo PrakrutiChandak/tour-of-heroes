2feceffd4751c41a721b24b907113e0918c71a90
PrakrutiChandak <prakrutichandak@gmail.com>
2020-08-27 20:14:38 +0530

    first commit

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Dr Strange' },
+    { id: 12, name: 'Deadpool' },
+    { id: 13, name: 'Hulk' },
+    { id: 14, name: 'Nick Furry' },
+    { id: 15, name: 'Captian' },
+    { id: 16, name: 'Iron Man' },
+    { id: 17, name: 'Spider Man' },
+    { id: 18, name: 'Black Widow' }
+];
